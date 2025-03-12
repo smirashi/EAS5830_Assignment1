5e8d90e702246d71381f1aef7788bb3b476e77c3
smirashi <56236178+smirashi@users.noreply.github.com>
2025-03-12 16:47:50 -0700

    Update connect_to_eth.py

--- connect_to_eth.py
+++ connect_to_eth.py
@@ -26,7 +26,7 @@ def connect_with_middleware(contract_json):
 
 	# TODO complete this method
 	# The first section will be the same as "connect_to_eth()" but with a BNB url
-	url = "wss://bsc-testnet.drpc.org"  # FILL THIS IN
+	url = "https://bsc-testnet.drpc.org"  # FILL THIS IN
 	w3 = Web3(HTTPProvider(url))
 	assert w3.is_connected(), f"Failed to connect to provider at {url}"
 
