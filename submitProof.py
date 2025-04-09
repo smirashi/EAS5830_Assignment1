10d2dd7854dab4684219eea399a26238b66f025f
smirashi <56236178+smirashi@users.noreply.github.com>
2025-04-09 05:53:59 -0700

    Update submitProof.py

--- submitProof.py
+++ submitProof.py
@@ -169,8 +169,8 @@ def send_signed_msg(proof, random_leaf):
     })
 
     # Sign and send the transaction
-    signed_tx = w3.eth.account.sign_transaction(tx, private_key=acct.key)
-    tx_hash = w3.eth.send_raw_transaction(signed_tx.rawTransaction)
+    signed_tx = eth_account.Account.sign_transaction(tx, acct.key)
+    tx_hash = w3.eth.send_raw_transaction(signed_tx['rawTransaction'])
 
     return tx_hash
 
