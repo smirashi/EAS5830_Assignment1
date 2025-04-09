823dfe5ae2e398ce8cdff43369f973bfda45ba6a
smirashi <56236178+smirashi@users.noreply.github.com>
2025-04-09 06:35:42 -0700

    Update Destination.sol

--- Destination.sol
+++ Destination.sol
@@ -25,40 +25,44 @@ contract Destination is AccessControl {
 	function wrap(address _underlying_token, address _recipient, uint256 _amount ) public onlyRole(WARDEN_ROLE) {
 		//YOUR CODE HERE
 		address wrapped = underlying_tokens[_underlying_token];
-		require(wrapped != address(0), "Token not registered");
-		
-		IERC20(_underlying_token).transferFrom(msg.sender, address(this), _amount);
-		BridgeToken(wrapped).mint(msg.sender, _amount);
+    		require(wrapped != address(0), "Wrapped token not registered");
+
+    		// Transfer original tokens from user to contract
+    		require(IERC20(_underlying_token).transferFrom(msg.sender, address(this), _amount), "Transfer failed");
+
+    		// Mint equivalent wrapped tokens to user
+    		BridgeToken(wrapped).mint(msg.sender, _amount);
 	}
 
 	function unwrap(address _wrapped_token, address _recipient, uint256 _amount ) public {
 		//YOUR CODE HERE
 		address underlying = wrapped_tokens[_wrapped_token];
-    		require(underlying != address(0), "Token not registered");
+    		require(underlying != address(0), "Underlying token not registered");
 
-    		// Burn the wrapped tokens from the user
-   		BridgeToken(_wrapped_token).burnFrom(msg.sender, _amount);
+    		// Burn the user's wrapped tokens
+    		BridgeToken(_wrapped_token).burnFrom(msg.sender, _amount);
 
-    		// Transfer back the underlying tokens
-    		IERC20(underlying).transfer(msg.sender, _amount);
+    		// Return original tokens to user
+    		require(IERC20(underlying).transfer(msg.sender, _amount), "Transfer failed");
 	}
 
 	function createToken(address _underlying_token, string memory name, string memory symbol ) public onlyRole(CREATOR_ROLE) returns(address) {
 		//YOUR CODE HERE
 		require(underlying_tokens[_underlying_token] == address(0), "Token already created");
 
-		BridgeToken newToken = new BridgeToken(
+    		// Destination contract will be the admin
+    		BridgeToken newToken = new BridgeToken(
         		_underlying_token,
         		name,
         		symbol,
-        		address(this) // this contract is the admin (has MINTER_ROLE)
+        		address(this)
     		);
 
-		// Save mappings both ways
+    		// Save mappings both ways
     		wrapped_tokens[address(newToken)] = _underlying_token;
     		underlying_tokens[_underlying_token] = address(newToken);
 
-    		return address(newToken);
+    		return address(newToken);;
 	}
 
 }
